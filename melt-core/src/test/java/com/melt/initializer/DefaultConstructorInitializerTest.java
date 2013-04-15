6fa0fe8434f5ec4c0c511761ceb9d96843f410a9
Haiyang He <hyhe@thoughtworks.com>
2013-04-15 21:51:59 +0800

    [haiyang]refactor the unit test

--- melt-core/src/test/java/com/melt/initializer/DefaultConstructorInitializerTest.java
+++ melt-core/src/test/java/com/melt/initializer/DefaultConstructorInitializerTest.java
@@ -5,6 +5,7 @@ import com.melt.beans.DefaultBankDao;
 import com.melt.config.BeanInfo;
 import com.melt.config.ConstructorFields;
 import com.melt.exceptions.InitBeanException;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
@@ -12,24 +13,26 @@ import static org.junit.Assert.assertThat;
 
 public class DefaultConstructorInitializerTest {
 
+    private DefaultConstructorInitializer initializer;
+
+    @Before
+    public void setUp() throws Exception {
+        initializer = new DefaultConstructorInitializer();
+    }
+
     @Test
     public void should_return_default_bankDao_bean() {
-        DefaultConstructorInitializer initializer = new DefaultConstructorInitializer();
         Object bean = initializer.initialize(new BeanInfo("bankDao", DefaultBankDao.class.getName()));
         assertThat(bean, instanceOf(BankDao.class));
     }
 
     @Test(expected = InitBeanException.class)
     public void should_throw_exception_when_bean_config_has_constructor_config() {
-        DefaultConstructorInitializer initializer = new DefaultConstructorInitializer();
-        Object bean = initializer.initialize(new BeanInfo("bankDao", new ConstructorFields(null)));
+        initializer.initialize(new BeanInfo("bankDao", new ConstructorFields(null)));
     }
 
     @Test(expected = InitBeanException.class)
     public void should_throw_exception_when_class_not_exist() {
-        DefaultConstructorInitializer initializer = new DefaultConstructorInitializer();
-        Object bean = initializer.initialize(new BeanInfo("bankDao", "notExistClass"));
+        initializer.initialize(new BeanInfo("bankDao", "notExistClass"));
     }
-
-
 }
