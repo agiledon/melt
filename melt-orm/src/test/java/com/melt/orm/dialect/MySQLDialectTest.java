39bbffd17acd3bb244c649dcc36977237dcd81d8
Haiyang He <hyhe@thoughtworks.com>
2013-05-20 13:49:43 +0800

    [haiyang] Add mysql dialect

---
+++ melt-orm/src/test/java/com/melt/orm/dialect/MySQLDialectTest.java
@@ -0,0 +1,24 @@
+package com.melt.orm.dialect;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MySQLDialectTest {
+    @Test
+    public void should_return_true_when_type_is_basic(){
+        MySQLDialect dialect = new MySQLDialect();
+        assertThat(dialect.isBasicType(Integer.class), is(true));
+        assertThat(dialect.isBasicType(Integer.TYPE), is(true));
+        assertThat(dialect.mappingFieldType(Integer.class), is("TINYINT UNSIGNED"));
+    }
+
+    @Test
+    public void should_return_false_when_type_is_not_basic(){
+        MySQLDialect dialect = new MySQLDialect();
+        assertThat(dialect.isBasicType(List.class), is(false));
+    }
+}
